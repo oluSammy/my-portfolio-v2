76824a81957f3066e44730a582d349496811f1f3
olusammy <olumorinsammy@gmail.com>
2025-01-20 06:35:31 +0100

    fix: about section import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Hero from "@/components/Hero/Hero";
 import Projects from "@/components/Sections/Projects/Projects";
-import About from "@/components/Sections/About/About";
+import About from "@/components/Sections/About";
 
 export default function Home() {
   return (
